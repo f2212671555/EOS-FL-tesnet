db489e005d969fa532248771ab042b5bf7b3283a
陸顥壬 <berserker@luhaorendeMacBook-Pro.local>
2021-04-23 23:34:39 +0800

    complete keosd container

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,3 +6,6 @@ docker volume create --name=wallet-data-volume
 echo "Building images"
 # docker-compose build --no-cache
 docker-compose build
+
+echo "Run all docker containers"
+docker-compose up -d
