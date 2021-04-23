54944d00ad347fe0a3d64586e9b1082787c422f0
陸顥壬 <berserker@luhaorendeMacBook-Pro.local>
2021-04-23 20:22:34 +0800

    add contracts.sh

--- services/eosio/scripts/contracts.sh
+++ services/eosio/scripts/contracts.sh
@@ -2,10 +2,23 @@
 
 cleos="cleos -u http://127.0.0.1:8000 --wallet-url http://127.0.0.1:6666"
 
-# deploy custom contracts
+# compile custom contract
+# $1 is where custom contract is
+function compile_contract() {
+    # generate wasm file, abi file
+    eosio-cpp $1.cpp -o $1.wasm
+}
+
+# recompile custom contract
+# $1 is where custom contract is
+function recompile_contract() {
+    eosio-cpp -abigen -o $1.wasm $1.cpp
+}
+
+# deploy custom contract
 # $1 is account name(which is created with 'system newaccount' and give it 'ram'),
-# $2 is contract name
-function deploy() {
+# $2 is where custom contract is
+function deploy_contract() {
     # set wasm
     $cleos set code $1 $2.wasm -p $1@active
     # set abi
